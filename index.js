f0843d6dc9a9a6be567e0aaf42a652d4984f9ed6
Kongkon Bora <kbora3525@gmail.com>
2025-03-08 01:14:52 +0530

    fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('./routes/User.js')
 const Transaction = require('./routes/Transactions.js')
 const Account= require('./routes/Accounts.js')
+const Profile = require('./routes/Financial_Profile.js')
 const app = express();
 
 app.use(express.json());
@@ -10,5 +11,6 @@ app.get("Hello there");
 app.use('/User',User);
 app.use('/Account',Account);
 app.use('/Transaction',Transaction);
+app.use('/Profile',Profile);
 
 app.listen(3000,()=>console.log('Server is online'));
